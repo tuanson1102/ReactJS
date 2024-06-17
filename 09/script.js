a514b7d5307b8f074963684c5bddf038f372fe65
TuanSon <tuanson06102k@gmail.com>
2024-06-18 00:25:50 +0700

    clone web 2

---
+++ 09/script.js
@@ -0,0 +1,5 @@
+let myName = 'thanh huyen';
+const MY_BIRTHDAY = '16/12/1993';
+
+console.log('tên tôi là', '2'*'3');
+console.log('tôi sinh ngày', MY_BIRTHDAY);    
---
+++ 10/script.js
@@ -0,0 +1,116 @@
+// thực thi 1 đoạn code nếu đk là đúng
+// const tired = false;
+// if (tired) {
+//     console.log('tôi sẽ nghỉ ngơi');
+//     console.log('tôi sẽ đá bóng');
+//     console.log('tôi sẽ chơi');
+// }
+
+// console.log('tiếp tục làm việc');
+// thực thi 1 trong 2 đoạn code tuỳ thuộc vào điều kiện
+// if (tired) {
+//     console.log('nghỉ ngơi');
+// } else {
+//     console.log('gấp đôi task');
+// }
+
+// console.log('làm việc');
+
+// các câu lệnh if else  lồng nhau
+
+// const mark = 55;
+// if (mark >= 85) {
+//   console.log("A");
+// } else if (mark >= 70 && mark < 85) {
+//   console.log("B");
+// } else if (mark >= 40 && mark < 70) {
+//   console.log("C");
+// } else {
+//   console.log("D");
+// }
+
+// const a = 10;
+// const b = 5;
+// if (a > b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+// const x = 0;
+// if (x > 0) {
+//   console.log("Dương");
+// } else if (x == 0) {
+//   console.log("Bằng 0");
+// } else {
+//   console.log("Âm");
+// }
+
+// function isEven(a, b, c) {
+//   if (c == b + a) {
+//     console.log("có ");
+//   } else {
+//     console.log("không");
+//   }
+// }
+
+// isEven(1, 2, 3);
+
+// function solveEquation(a, b, c) {
+//   // Code
+//   let data;
+//   if (a == 0) {
+//     if (b == 0) {
+//       if (c == 0) {
+//         data = Infinity;
+//       } else {
+//         data = Infinity;
+//       }
+//     } else {
+//       x = -c / b;
+//       data = x;
+//     }
+//   } else {
+//     dt = b * b - 4 * a * c;
+//     if (dt < 0) {
+//       data = null;
+//     } else if (dt == 0) {
+//       x = -b / (2 * a);
+//       data = x;
+//     } else {
+//       x1 = (-b + Math.sqrt(dt)) / (2 * a);
+//       x2 = (-b - Math.sqrt(dt)) / (2 * a);
+//       data = [x1, x2];
+//     }
+//   }
+//   return data; // Kết quả
+// }
+//  console.log(`Phương trình có số nghiệm là:`,solveEquation(1,2,1));
+
+// Switch case
+// kiểm tra giá trị, và tuỳ thuộc vào trường hợp cụ thể của giá trị
+// để thực hiện khối lệnh
+// function printNumber() {
+//   for (i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// console.log("Dãy số chẵn trong khoảng từ 1 - 100");
+// printNumber();
+
+// function totalNumber(n) {
+//   let sum = 0;
+//   for ( let i = 1; i <= n; i++) {
+//     if (i % 15 == 0) {
+//       console.log( sum += i);
+//     }
+//   }
+// }
+
+// console.log("Dãy số chẵn trong khoảng từ 1 - 100");
+// totalNumber(50);
+
+
